bd9a85fcd490706be5c2f89ebec36911ab24ec82
Eduardo Barron <eduard_park.13@hotmail.com>
2019-06-30 14:47:50 -0500

    Comenzando con Vuex

--- components/hijo.js
+++ components/hijo.js
@@ -3,8 +3,17 @@ Vue.component('hijo', {
     `
     <div class="py-5 bg-success">
         <h3>Componente hijo</h3>
-        <h5>{{numero}}</h5>
+        <h4>numero: {{numero}}</h4>
+        <h4>Nombre: {{name}}</h4>
     </div>
     `,
-    props: ['numero']
+    props: ['numero'],
+    data(){
+        return{
+            name: "Alberto"
+        }
+    },
+    mounted(){
+        this.$emit('nameHijo', this.name);
+    }
 })
--- components/padre.js
+++ components/padre.js
@@ -4,12 +4,14 @@ Vue.component('padre', {
     <div class="p-5 bg-dark text-white">
         <h2>Componente padre</h2>
         <button class="btn btn-danger" @click="numeroPadre++">+</button>
-        <hijo :numero="numeroPadre"></hijo>
+        <hijo :numero="numeroPadre" @nameHijo="nameFromSon = $event"></hijo>
+        <h3>Nombre desde hijo: {{nameFromSon}}</h3>
     </div>
     `,
     data(){
         return{
-            numeroPadre: 0
+            numeroPadre: 0,
+            nameFromSon:''
         }
     }
 })
